67ee3a4344159f376415172ba0fc440b2573cfe2
Alexis Vargas <u202113256@upc.edu.pe>
2024-11-19 17:31:33 -0500

    feat: publishing on the develop branch regarding the lifecycle API

---
+++ src/main/java/com/example/agricorp/lifecycle/LifecycleApplication.java
@@ -0,0 +1,11 @@
+package com.example.agricorp.lifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifecycleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LifecycleApplication.class, args);
+    }
+}
