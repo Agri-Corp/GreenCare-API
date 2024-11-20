3a543c79ebe75d1e36be8397f9d278ada96b62f1
José Diego <JoseHuamani_20@hotmail.com>
2024-11-20 01:53:35 -0500

    fix: refactor the integration of lifecycle and garden layers into the DDD of our API

--- src/main/java/com/example/agricorp/gardenManagement/controllers/GardenController.java
+++ src/main/java/com/example/agricorp/gardenManagement/controllers/GardenController.java
@@ -3,6 +3,7 @@ package com.example.agricorp.gardenManagement.controllers;
 
 import com.example.agricorp.gardenManagement.models.Garden;
 import com.example.agricorp.gardenManagement.services.GardenService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/garden")
+@RequestMapping("/api/v1/garden")
 public class GardenController {
 
     private final GardenService gardenService;
@@ -18,9 +19,9 @@ public class GardenController {
     public GardenController(GardenService gardenService) {
         this.gardenService = gardenService;
     }
-
     @GetMapping
-    public List<Garden> getAllGardens() {
-        return gardenService.getAllGardens();
+    public ResponseEntity<List<Garden>> getAllGardens() {
+        List<Garden> gardens = gardenService.getAllGardens();
+        return ResponseEntity.ok(gardens);
     }
 }
